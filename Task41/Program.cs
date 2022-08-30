9b54a582873efa8c09cf9743413c959ec51f6b02
MikleBuntin <BuntinMA@mail.ru>
2022-08-30 13:58:01 +0300

    Решил задачу 41, правда без массивов, рекурсии и функций.

--- Task41/Program.cs
+++ Task41/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите количество чисел: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int k = 0;
+for(int i = 0; i < n; i++)
+{
+    Console.WriteLine("Введите очередное число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    if(number > 0) k = k + 1;
+}
+Console.WriteLine($"Количество положителных чисел: " + k);
